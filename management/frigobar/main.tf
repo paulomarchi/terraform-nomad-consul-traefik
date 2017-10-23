9d52946ddca6865c471e564066b380ec1a6dc0b4
Paulo Marchi <paulo.marchi@gmail.com>
2017-10-23 18:02:29 -0200

    frigobar: fix variables
    Fix variables profile and regions

--- management/frigobar/main.tf
+++ management/frigobar/main.tf
@@ -1,33 +1,22 @@
 # Specify the provider and access details
 provider "aws" {
-  region = "${var.aws_region}"
+  region  = "${var.aws_region}"
   profile = "${var.aws_profile}"
 }
 
-data "aws_ami" "standard" {
-  most_recent = true
-  owners      = ["self"]
-
-  filter {
-    name   = "name"
-    values = ["${var.image-search-path}"]
-  }
-}
-
 module "frigobar-sg" {
-  source    = "../security-groups"
-  namespace = "${var.namespace}"
-  profile   = "${var.aws_profile}"
-  region    = "${var.aws_region}"
-  vpc_id    = "${var.vpc_id}"
+  source      = "../security-groups"
+  aws_profile = "${var.aws_profile}"
+  aws_region  = "${var.aws_region}"
+  vpc_id      = "${var.vpc_id}"
 }
 
 # Create Userdata 
 module "frigobar-init" {
   source        = "../userdata"
   namespace     = "${var.namespace}"
-  profile       = "${var.aws_profile}"
-  region        = "${var.aws_region}"
+  aws_profile   = "${var.aws_profile}"
+  aws_region    = "${var.aws_region}"
   nomad_prefix  = "${var.nomad_prefix}"
   nomad_region  = "${var.nomad_region}"
   cluster_size  = "${var.cluster_size}"
@@ -38,22 +27,18 @@ module "frigobar-init" {
 
 # Create Instance
 module "cluster-instance" {
-  source             = "../instance"
-  userdata           = "${module.frigobar-init.userdata}"
-  name_prefix        = "frigobar-${var.type_infra}"
-  namespace          = "${var.namespace}"
-  vpc_id             = "${var.vpc_id}"
-  security_groups    = "${module.frigobar-sg.id}"
-  profile            = "${var.aws_profile}"
-  instance_type      = "${var.instance_type}"
-  region             = "${var.aws_region}"
-  key_name           = "${var.key_name}"
-  cluster_size       = "${var.cluster_size}"
-  subnet_ids         = "${var.subnet_ids}"
-  ec2_tag_value      = "${var.ec2_tag_value}"
-  image-search-path  = "${data.aws_ami.standard.id}"
+  source            = "../instance"
+  userdata          = "${module.frigobar-init.userdata}"
+  name_prefix       = "frigobar-${var.type_infra}"
+  namespace         = "${var.namespace}"
+  vpc_id            = "${var.vpc_id}"
+  security_groups   = "${module.frigobar-sg.id}"
+  aws_profile       = "${var.aws_profile}"
+  instance_type     = "${var.instance_type}"
+  aws_region        = "${var.aws_region}"
+  key_name          = "${var.key_name}"
+  cluster_size      = "${var.cluster_size}"
+  subnet_ids        = "${var.subnet_ids}"
+  ec2_tag_value     = "${var.ec2_tag_value}"
+  image-search-path = "${var.image-search-path}"
 }
-
-
-
-
