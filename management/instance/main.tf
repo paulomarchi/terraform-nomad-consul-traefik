ad3db105d1de805eb89faf82adc76c7802953e13
Paulo Marchi <paulo.marchi@gmail.com>
2017-10-25 14:08:19 -0200

    frigobar: Add module
    Add module role (permissions) aws for instance

--- management/instance/main.tf
+++ management/instance/main.tf
@@ -13,14 +13,22 @@ data "aws_ami" "standard" {
   }
 }
 
+module "role" {
+  source      = "../role"
+  namespace   = "${var.name_prefix}-profile-${var.namespace}"
+  aws_profile = "${var.aws_profile}"
+  aws_region  = "${var.aws_region}"
+}
+
 resource "aws_instance" "node" {
-  count           = "${var.cluster_size}"
-  ami             = "${data.aws_ami.standard.id}"
-  instance_type   = "${var.instance_type}"
-  subnet_id       = "${element(var.subnet_ids, count.index)}"
-  security_groups = ["${var.security_groups}"]
-  key_name        = "${var.key_name}"
-  user_data       = "${var.userdata}"
+  count                = "${var.cluster_size}"
+  ami                  = "${data.aws_ami.standard.id}"
+  instance_type        = "${var.instance_type}"
+  subnet_id            = "${element(var.subnet_ids, count.index)}"
+  security_groups      = ["${var.security_groups}"]
+  key_name             = "${var.key_name}"
+  user_data            = "${var.userdata}"
+  iam_instance_profile = "${module.role.iam_instance_profile}"
 
   root_block_device {
     volume_type = "gp2"
