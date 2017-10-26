162c21083c92c9c04b7bfee442b4a0ea61f4fda3
Paulo Marchi <paulo.marchi@gmail.com>
2017-10-26 15:28:19 -0200

    Add new lines in output

--- management/_structure_init/outputs.tf
+++ management/_structure_init/outputs.tf
@@ -3,9 +3,21 @@ output "server_instance_ids" {
 }
 
 output "server_address_instance_private" {
-  value = "${module.server.private_ips}"
+  value = "${module.server.address_instance_private}"
 }
 
 output "server_address_instance_public_ip" {
-  value = "${module.server.public_ips}"
+  value = "${module.server.address_instance_public_ip}"
+}
+
+output "client_instance_ids" {
+  value = "${module.client.instance_ids}"
+}
+
+output "client_address_instance_private" {
+  value = "${module.client.address_instance_private}"
+}
+
+output "client_address_instance_public_ip" {
+  value = "${module.client.address_instance_public_ip}"
 }
